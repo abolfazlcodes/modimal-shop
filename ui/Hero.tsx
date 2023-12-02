a7e5a7d618737b3ecfa623ba25a9b70507051534
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2023-12-02 18:12:38 +0330

    add hero and button components

---
+++ ui/Hero.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import { League_Spartan } from "next/font/google";
+import Button from "./Button";
+
+const LeagueSpartan = League_Spartan({ subsets: ["latin"] });
+
+const Hero = () => {
+  return (
+    <section className="w-full h-[30rem] lg:h-[45rem] relative">
+      <Image
+        src="/hero.png"
+        fill
+        alt="banner image"
+        quality={90}
+        priority
+        className="h-full w-full object-cover object-center"
+      />
+
+      <div
+        className={`absolute bottom-10 lg:bottom-40 left-5 lg:left-20 px-6 py-4 animate-in ${LeagueSpartan.className}`}
+      >
+        <h1 className="italic text-2xl lg:text-3xl text-white font-bold tracking-wide mb-2">
+          Elegance In Simplicity,
+        </h1>
+        <h1 className="italic text-2xl lg:text-3xl text-white font-bold tracking-wide mb-4 lg:mb-8">
+          Earth&apos;s Harmony
+        </h1>
+
+        <Button type="primary" href="/products">
+          New In
+        </Button>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
